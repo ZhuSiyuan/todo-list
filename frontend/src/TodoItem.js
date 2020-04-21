802800bffac383a2f057c9b77c35c9b86ac1d93c
liangqiLQ <2608186018@qq.com>
2020-04-21 17:27:58 +0800

    -mtodolist删改组件

--- frontend/src/TodoItem.js
+++ frontend/src/TodoItem.js
@@ -6,7 +6,7 @@ const TodoItem = ({ item, onItemUpdate, onItemDelete }) => {
   const [todoItem, setTodoItem] = useState(item);
 
   return (
-    <li className="task-item" id={"task-item-" + todoItem.id} data-testid="task-item">
+    <li align='center' className="task-item" id={"task-item-" + todoItem.id} data-testid="task-item">
       <TextField
         value={todoItem.content}
         disabled={!isEditable}
@@ -24,14 +24,14 @@ const TodoItem = ({ item, onItemUpdate, onItemDelete }) => {
         onClick={() => setIsEditable(!isEditable)}
         data-testid="edit-button"
       >
-        Edit
+        修改
       </span>
       <span
         className="text-button delete-button"
         onClick={() => onItemDelete(item.id)}
         data-testid="delete-button"
       >
-        Delete
+        删除
       </span>
     </li>
   );
