7f2100405cf3efcfa9767d16fe1ffbf80479e8c2
ZhuSiyuan <iamyournaturalfather@gmail.com>
2020-04-22 16:16:06 +0800

    Fix Bug (CORS error)
    For the CORS error:
    "Access to fetch at 'http://127.0.0.1:8080/api/tasks/' (redirected from 'http://localhost:3000/api/tasks') from origin 'http://localhost:3000' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled."

--- backend/controller.py
+++ backend/controller.py
@@ -6,25 +6,36 @@
 '''
 
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 from service import TaskService
 
 JSON_AS_ASCII = False
 app = Flask(__name__)
 
+def do_response(data):
+    response = make_response(data)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization,Content-type,Accept,X-Requested-With,sid,mycustom,smuser,Origin'
+    response.headers['Access-Control-Max-Age'] = '3600'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Cache-Control'] = 'no-cache'
+    return response
+
 
 # index page
 @app.route('/', methods=['GET'])
 def index():
-    return 'hello world', 200
+    return do_response('hello world'), 200
 
 
 # 返回所有Todo任务
 @app.route('/api/tasks/', methods=['GET'])
 def query_all():
     tasks = TaskService.query_all()
-    return jsonify(tasks), 200
+
+    return do_response(jsonify(tasks)), 200
 
 
 # 返回一个指定ID的Todo任务
@@ -33,29 +44,32 @@ def query_by_id(id=None):
     try:
         id = int(id)
     except:
-        return '"id" must be a integer', 403
+        return do_response('"id" must be a integer'), 403
 
     task = TaskService.query_by_id(id)
     if task is None:
-        return 'Not found, no "id=%s" item in database' % (id), 404
+        return do_response('Not found, no "id=%s" item in database' % (id)), 404
     else:
-        return jsonify(task)
+        return do_response(jsonify(task)), 200
 
 
 # 创建一个新的Todo任务
 @app.route('/api/tasks/', methods=['POST'])
 def insert():
+    print(request.get_data())
     task_insert = str(request.get_data(), encoding='utf-8')
     try:
         task_insert = json.loads(task_insert)
     except:
-        return 'Please check the validity of the data', 403
+        return do_response('Please check the validity of the data'), 403
+
+    print(task_insert)
 
     state, msg = TaskService.insert(task_insert)
     if state is False:
-        return msg, 403
+        return do_response(msg), 403
     else:
-        return jsonify(msg), 200
+        return do_response(jsonify(msg)), 200
 
 
 # 更新一个新的Todo任务
@@ -65,13 +79,13 @@ def update():
     try:
         task_update = json.loads(task_update)
     except:
-        return 'Please check the validity of the data', 403
+        return do_response('Please check the validity of the data'), 403
 
     state, msg = TaskService.update(task_update)
     if state is False:
-        return msg, 403
+        return do_response(msg), 403
     else:
-        return jsonify(msg), 200
+        return do_response(jsonify(msg)), 200
 
 
 # 删除一个Todo任务
@@ -80,13 +94,13 @@ def delete_by_id(id=None):
     try:
         id = int(id)
     except:
-        return '"id" must be a integer', 403
+        return do_response('"id" must be a integer'), 403
 
     state, msg = TaskService.delete_by_id(id)
     if state is False:
-        return msg, 404
+        return do_response(msg), 404
     else:
-        return jsonify(msg), 200
+        return do_response(jsonify(msg)), 200
 
 
 if __name__ == '__main__':
